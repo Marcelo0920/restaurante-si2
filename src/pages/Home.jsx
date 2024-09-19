2779bafb90dc73ddc2042b841c3795f012d54d0e
Marcelo0920 <marcelo0920.z@gmail.com>
2024-09-19 00:34:40 -0400

    se agrego las publicaciones en la pagina home, junto a un pagination para navegar entre los platos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Dashboard from "../components/dashboard/Dashboard";
+import ProductsHome from "../components/products/ProductsHome";
 
 const Home = () => {
   return (
     <div className="sections-wrapper">
       <Dashboard />
-      Home
+      <ProductsHome />
     </div>
   );
 };
